e013fbf12c2bfd6701ebfb5d35d3a710c063d9b6
agusrichard <agus.richard21@gmail.com>
2020-03-22 11:06:53 +0700

    Change cart and checkout

--- src/pages/cartAndCheckout/CartAndCheckout.js
+++ src/pages/cartAndCheckout/CartAndCheckout.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
-import { Container } from 'reactstrap'
+import { Container, Form, FormGroup, Label } from 'reactstrap'
 import { connect } from 'react-redux'
+import Slide from 'react-reveal/Slide'
 import { removeItemFromCart } from '../../redux/actions/user'
 import CustomNavbar from '../../components/CustomNavBar'
 import Footer from '../../components/Footer'
@@ -11,6 +12,30 @@ import RemoveIcon from '../../assets/images/icons/trash.svg'
 import '../../assets/styles/cart.css'
 
 class CartAndCheckout extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      processStatus: 0,
+      isOpen: false,
+      fullname: this.props.userData.full_name,
+      address: this.props.userData.address
+    }
+  }
+
+  handleChangeStatus = (status) => {
+    this.setState({ processStatus: status })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleCheckout = () => {
+
+  }
+
   render() {
     const rendered = this.props.itemsInCart.map(item => (
       <tr className="cart-table-row">
@@ -43,35 +68,70 @@ class CartAndCheckout extends Component {
           <h3 className="text-center restaurant-list-main-text mb-2">Your selected items</h3>
           <hr className="heading-hr mb-5" />
         </div>
-        <Container className="mb-3" style={{ overflowX: 'auto' }}>
+        <Container className="mb-3">
         { this.props.itemsInCart.length === 0 ?
           <p className="text-center">No items in your cart</p> :
         <>
-          <div className="text-center mb-5">
-            <span className="checkout-process-btn">Cart</span>
-            <span className="checkout-process-btn">Checkout</span>
-            <span className="checkout-process-btn">Success</span>
-          </div>
-          <table className="cart-table">
-            <thead>
-              <tr>
-              <th className="cart-table-head">Actions</th>
-              <th className="cart-table-head">Item Name</th>
-              <th className="cart-table-head">Quantity</th>
-              <th className="cart-table-head">Price</th>
-              <th className="cart-table-head">Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              { rendered }
-              <td className="cart-table-head"></td>
-              <td className="cart-table-head"></td>
-              <td className="cart-table-head"></td>
-              <td className="cart-table-head">Expenses</td>
-              <td className="cart-table-head">Rp. {expenses}</td>
-            </tbody>
-            </table>
-            <button className="text-center">Ready to Checkout</button>
+          <div style={{ overflowX: 'auto' }}>
+            <div className="text-center mb-5">
+              <span className={ this.state.processStatus !== 0 ? "checkout-process-btn" : "checkout-process-btn-active"} onClick={() => this.handleChangeStatus(0)}>Cart</span>
+              <span className={ this.state.processStatus !== 1 ? "checkout-process-btn" : "checkout-process-btn-active"} onClick={() => this.handleChangeStatus(1)}>Checkout</span>
+              <span className={ this.state.processStatus !== 2 ? "checkout-process-btn" : "checkout-process-btn-active"} onClick={() => this.handleChangeStatus(2)}>Success</span>
+            </div>
+            { this.state.processStatus === 0 ?
+            <Slide right>
+              <table className="cart-table">
+              <thead>
+                <tr>
+                  <th className="cart-table-head">Actions</th>
+                  <th className="cart-table-head">Item Name</th>
+                  <th className="cart-table-head">Quantity</th>
+                  <th className="cart-table-head">Price</th>
+                  <th className="cart-table-head">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                { rendered }
+                <tr>
+                  <td className="cart-table-bottom"></td>
+                  <td className="cart-table-bottom"></td>
+                  <td className="cart-table-bottom"></td>
+                  <td className="cart-table-bottom">Expenses</td>
+                  <td className="cart-table-bottom">Rp. {expenses}</td>
+                </tr>
+              </tbody>
+              </table>
+            </Slide> :
+              this.state.processStatus === 1 ?
+              <Slide right>
+                <div className="checkout-form">
+                  <Form>
+                  <FormGroup>
+                    <Label for="fullname" className="create-restaurant-form-label">Your Name</Label>
+                    <input type="text" className="create-restaurant-form-input" name="fullname" id="fullname" placeholder={this.props.userData.full_name} onChange={this.handleChange}/>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="address" className="create-restaurant-form-label">Please provide your address</Label>
+                    <textarea className="create-restaurant-form-input" name="description" id="address" onChange={this.handleChange} placeholder={this.props.userData.address}/>
+                  </FormGroup>
+                </Form>
+                </div>
+              </Slide>
+              : <p>something</p>
+            }
+            </div>
+            <div className="btn-container text-center">
+            { this.state.processStatus === 0 ?
+              <button className="ready-btn" onClick={() => this.handleChangeStatus(this.state.processStatus + 1)}>Ready To Checkout</button> : 
+              <>
+                <button className="ready-btn" onClick={() => {
+    
+                }}>Checkout</button>
+                <div className="custom-modal" style={ this.state.isOpen ? {display: 'block'} : null }>
+                </div>
+              </>
+            }
+            </div>
           </>
           }
         </Container>
@@ -81,7 +141,10 @@ class CartAndCheckout extends Component {
   }
 }
 
-const mapStatetoProps = state => ({ itemsInCart: state.user.itemsInCart })
+const mapStatetoProps = state => ({ 
+  itemsInCart: state.user.itemsInCart,
+  userData: state.auth.userData
+})
 const mapDispatchToProps = { removeItemFromCart }
 
 export default connect(mapStatetoProps, mapDispatchToProps)(CartAndCheckout)
